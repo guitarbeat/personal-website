6c55c97d8f8c03f208620dbe2944f79af345be3c
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2026-09-03 20:24:29 +0000

    remove unused setAudioVolume function from audioUtils

--- src/utils/audioUtils.ts
+++ src/utils/audioUtils.ts
@@ -413,8 +413,6 @@ const audioManager = new AudioManager();
 // Export functions for easy use
 export const playKnightRiderTheme = () => audioManager.playKnightRiderTheme();
 export const stopKnightRiderTheme = () => audioManager.stop();
-export const setAudioVolume = (volume: number) =>
-  audioManager.setVolume(volume);
 export const isAudioPlaying = () => audioManager.getPlayingState();
 export const cleanupAudio = () => audioManager.cleanup();
 
