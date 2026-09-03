6b2857db8ac6193d9032685e3f6204a4cc2848c1
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2026-09-03 20:29:51 +0000

    chore: remove unused isAudioPlaying export from audioUtils

--- src/utils/audioUtils.ts
+++ src/utils/audioUtils.ts
@@ -415,7 +415,6 @@ export const playKnightRiderTheme = () => audioManager.playKnightRiderTheme();
 export const stopKnightRiderTheme = () => audioManager.stop();
 export const setAudioVolume = (volume: number) =>
   audioManager.setVolume(volume);
-export const isAudioPlaying = () => audioManager.getPlayingState();
 export const cleanupAudio = () => audioManager.cleanup();
 
 export default audioManager;
