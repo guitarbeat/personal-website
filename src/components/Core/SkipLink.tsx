791070e4e8cab0d2bcb3f00ff5d583362c5d2ab7
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2026-09-05 14:37:49 -0500

    feat(a11y): add skip link and update focus indicators (#1053)

---
+++ src/components/Core/SkipLink.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SkipLink.scss";
+
+const SkipLink: React.FC = () => {
+  return (
+    <a href="#main-content" className="skip-link">
+      Skip to main content
+    </a>
+  );
+};
+
+export default SkipLink;
